659ccd2d98f9254de8ac6d956dfd6ea75556ee22
Patrik Rufino <patrik.rufino@gmail.com>
2021-05-17 15:55:54 -0300

    Criando telas de login, cadastro e inicio

---
+++ View/primeiro-acesso.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="http://localhost/projeto-final-etec/cadastro-de-curriculos/resources/styles/style.css">
+
+    <title>Elatados Juarez</title>
+</head>
+
+<body>
+    <form action="" method="POST" class="w3-container w3-card-4 w3-light-grey w3-text-blue w3-margin w3-display-middle" style="width: 30%;">
+        <h2 class="w3-center">Primeiro Acesso</h2>
+        <div class="w3-row w3-section">
+            <div class="w3-col center-elements" style="width: 11%;">
+                <i class="w3-xxlarge fa fa-user"></i>
+            </div>
+            <div class="w3-rest">
+                <input type="text" class="w3-input w3-border w3-round-large" name="txtNome" placeholder="Nome Completo">
+            </div>
+        </div>
+
+        <div class="w3-row w3-section ">
+            <div class="w3-col center-elements" style="width: 11%;">
+                <i class="w3-xxlarge fa fa-drivers-license"></i>
+            </div>
+            <div class="w3-rest">
+                <input type="text" class="w3-input w3-border w3-round-large" name="txtCPF" placeholder="CPF: 333333333333">
+            </div>
+        </div>
+
+        <div class="w3-row w3-section">
+            <div class="w3-col center-elements" style="width: 11%;">
+                <i class="w3-xxlarge fa fa-envelope-o"></i>
+            </div>
+            <div class="w3-rest">
+                <input type="text" class="w3-input w3-border w3-round-large" name="txtEmail" placeholder="Email">
+            </div>
+        </div>
+
+        <div class="w3-row w3-section">
+            <div class="w3-col center-elements" style="width: 11%;">
+                <i class="w3-xxlarge fa fa-lock"></i>
+            </div>
+            <div class="w3-rest">
+                <input type="password" class="w3-input w3-border w3-round-large" name="txtSenha" placeholder="Senha">
+            </div>
+        </div>
+
+        <div class="w3-row w3-section">
+            <div class="w3-center">
+                <a href="http://localhost/projeto-final-etec/cadastro-de-curriculos/View/login.php">
+                    <button name="btnCadastrar" style="width: 90%;" class="w3-button w3-block w3-margin w3-blue w3-cell w3-round-large">
+                        Cadastrar
+                    </button>
+                </a>
+            </div>
+        </div>
+    </form>
+</body>
+
+</html>
---
+++ View/principal.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="http://localhost/projeto-final-etec/cadastro-de-curriculos/resources/styles/style.css">
+
+    <title>Elatados Juarez</title>
+</head>
+
+<body class="w3-light-grey">
+    <nav class="w3-sidebar w3-bar block w3-center w3-blue">
+        <a href="#home" class="w3-bar-item w3-button w3-block w3-cell w3-hover-light-grey w3-hover-text-cyan w3-text-light-grey">
+            <i class="fa fa-home w3-xxlarge"></i>
+            <p> Tela Principal </p>
+        </a>
+        <a href="#dPessoais" class="w3-bar-item w3-button w3-block w3-cell w3-hover-light-grey w3-hover-text-cyan w3-text-light-grey">
+            <i class="fa fa-address-book-o w3-xxlarge"></i>
+            <p>Dados Pessoais</p>
+        </a>
+        <a href="#formacao" class="w3-bar-item w3-button w3-block w3-cell w3-hover-light-grey w3-hover-text-cyan w3-text-light-grey">
+            <i class="fa fa-mortar-board w3-xxlarge"></i>
+            <p>Formação Acadêmica</p>
+        </a>
+    </nav>
+
+    <div class="w3-padding-large" id="main"> </div>
+</body>
+
+</html>
