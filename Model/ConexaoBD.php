938d63bfd133b0c1aa4f0e87d001b7d21d5f14d8
Patrik Rufino <patrik.rufino@gmail.com>
2021-06-05 21:03:04 -0300

    Adicionando opção de bd de produção

--- Model/ConexaoBD.php
+++ Model/ConexaoBD.php
@@ -1,16 +1,33 @@
 <?php
 
+// Development
+//class ConexaoBD{
+//
+//    private $servername = "localhost";
+//    private $username = "root";
+//    private $password = "";
+//    private $dbname = "projeto_final";
+//
+//    public function conectar()
+//    {
+//        $conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+//        return $conn;
+//    }
+//
+//}
+
+// Production
 class ConexaoBD{
-    
+
     private $servername = "localhost";
-    private $username = "root";
-    private $password = "";
-    private $dbname = "projeto_final";
+    private $username = "id16982950_juarezenlatados";
+    private $password = "3{!ID2O8rme/[wi9";
+    private $dbname = "id16982950_projeto_final";
 
     public function conectar()
     {
         $conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
         return $conn;
-    }   
+    }
 
 }
